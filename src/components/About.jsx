13acdbc4eb499a2e701068ee8d04abfe8d6c1a63
Will Thirkettle <william.thirkettle@ubs.com>
2017-08-04 16:51:24 +0100

    fonts update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ function About() {
     <div className={styles.page}>
       <Header className={styles.header} />
       <div className={styles.contents}>
-        <h3 className={classnames(typography.margins, typography.phil)}>Choosy Movie?</h3>
+        <h3 className={classnames(typography.margins, typography.will)}>Choosy Movie?</h3>
         <p className={classnames(typography.bottomMargin, typography.harrison)}>
           At your service! Helps you pick a movie to watch based
           on what you‘re in the mood for right now...
@@ -23,22 +23,22 @@ function About() {
           src={logo}
           alt="Choosy Movie"
         />
-        <h3 className={classnames(typography.margins, typography.phil)}>How?</h3>
+        <h3 className={classnames(typography.margins, typography.will)}>How?</h3>
         <p className={classnames(typography.bottomMargin, typography.harrison)}>
           Simple. Select what you are in the mood for and hit the button.
           We hope you like the movie suggestions!
         </p>
-        <h3 className={classnames(typography.margins, typography.phil)}>Why?</h3>
+        <h3 className={classnames(typography.margins, typography.will)}>Why?</h3>
         <p className={classnames(typography.bottomMargin, typography.harrison)}>
           Mainly for a bit of fun, but also because I found it hard to find a film to watch on
           Netflix and Amazon based on how I was feeling
         </p>
-        <h3 className={classnames(typography.margins, typography.phil)}>Who?</h3>
+        <h3 className={classnames(typography.margins, typography.will)}>Who?</h3>
         <p className={classnames(typography.bottomMargin, typography.harrison)}>
           My name is Will Thirkettle - I‘m a developer - you can find me
           here: <a title="Will Thirkettle - Software Engineer" href="http://willthirkettle.co.uk/">http://willthirkettle.co.uk/</a>.
         </p>
-        <h3 className={classnames(typography.margins, typography.phil)}>Who else?</h3>
+        <h3 className={classnames(typography.margins, typography.will)}>Who else?</h3>
         <p className={classnames(typography.bottomMargin, typography.harrison)}>
           The data used in this app (Choosy Movie) is primarily sourced from the
           excellent <a title="The Movie DB" href="https://www.themoviedb.org/">TMDb APIs</a>.
@@ -55,7 +55,7 @@ function About() {
           This app also uses Netflix Roulette API and iTunes Search API
           to access availability of the movie results on the two platforms.
         </p>
-        <h3 className={classnames(typography.margins, typography.phil)}>Thanks</h3>
+        <h3 className={classnames(typography.margins, typography.will)}>Thanks</h3>
         <p className={classnames(typography.bottomMargin, typography.harrison)}>
           Special thanks to my beautiful wife Vicky, and my two lovely boys Harrison and Elliot.
         </p>
