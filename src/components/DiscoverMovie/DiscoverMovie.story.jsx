e6c113782e6f306ddf657cd327561efec26f8ab8
Will Thirkettle <hello@willthirkettle.co.uk>
2017-08-07 18:21:53 +0100

    Add DiscoverMovie story

---
+++ src/components/DiscoverMovie/DiscoverMovie.story.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Immutable from 'immutable';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { DiscoverMovie } from './DiscoverMovie';
+import Story from '../../storybook/story';
+import MOODS from '../../constants/moods';
+// import styles from '../../storybook/story.css';
+
+const getSelected = () => {
+  const selected = [];
+  Object.keys(MOODS).forEach((key) => {
+    if (boolean(key, false)) {
+      selected.push(key);
+    }
+  });
+  return Immutable.List(selected);
+};
+
+const getProps = () => ({
+  track: () => {},
+  requestMovies: () => {},
+  requestSetMood: () => {},
+  genreGroups: [],
+  moodsKey: getSelected().size ? 'something' : '',
+  moodsSelected: getSelected()
+});
+
+storiesOf('DiscoverMovie', module)
+  .addDecorator(withKnobs)
+  .add('view', () => (
+    <Story
+      title="DiscoverMovie"
+      summary="View which displays the mood options plus actions"
+      displayInfo={boolean('displayInfo', false)}
+    >
+      <DiscoverMovie {...getProps()} />
+    </Story>
+  ));
