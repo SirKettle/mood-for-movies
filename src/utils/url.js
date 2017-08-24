025183b056a4880d7b9b1daca98d6bd44da97af8
Will Thirkettle <hello@willthirkettle.co.uk>
2017-08-24 21:05:10 +0100

    add util funds

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,5 @@
-const getUriSafe = str => str.replace(/ /gi, '-').replace(/[^A-Za-z0-9_-]/gi, '');
+export const getUriSafe = str => str.replace(/ /gi, '-').replace(/[^A-Za-z0-9_-]/gi, '');
 
-export default getUriSafe;
+export const encodeUriSafe = str => str.replace(/ /gi, '_').replace(/[^A-Za-z0-9_-]/gi, '');
+
+export const decodeUriSafe = str => str.replace(/_/gi, ' ');
